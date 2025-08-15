3a54a79abfe92d09c54c40de9d34c146c9fcd4d3
d_ruiz_d <druz@icloud.com>
2025-08-15 11:54:31 -0700

    feat(react): top Navbar and layout polish (sticky, responsive links)

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -8,21 +8,25 @@ import WebsitesPage from './pages/WebsitesPage'
 import ContentPage from './pages/ContentPage'
 import ApiSettingsPage from './pages/ApiSettingsPage'
 import BrandVoicePage from './pages/BrandVoicePage'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/home_page" element={<HomePage />} />
-      <Route path="/roadmap_page" element={<RoadmapPage />} />
-      <Route path="/article_editor_page" element={<ArticleEditorPage />} />
-      <Route path="/article_builder" element={<ArticleBuilderPage />} />
-      <Route path="/websites_page" element={<WebsitesPage />} />
-      <Route path="/content_page" element={<ContentPage />} />
-      <Route path="/api_settings" element={<ApiSettingsPage />} />
-      <Route path="/brand_voice" element={<BrandVoicePage />} />
-      <Route path="/" element={<Navigate to="/home_page" replace />} />
-      <Route path="*" element={<Navigate to="/home_page" replace />} />
-    </Routes>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/home_page" element={<HomePage />} />
+        <Route path="/roadmap_page" element={<RoadmapPage />} />
+        <Route path="/article_editor_page" element={<ArticleEditorPage />} />
+        <Route path="/article_builder" element={<ArticleBuilderPage />} />
+        <Route path="/websites_page" element={<WebsitesPage />} />
+        <Route path="/content_page" element={<ContentPage />} />
+        <Route path="/api_settings" element={<ApiSettingsPage />} />
+        <Route path="/brand_voice" element={<BrandVoicePage />} />
+        <Route path="/" element={<Navigate to="/home_page" replace />} />
+        <Route path="*" element={<Navigate to="/home_page" replace />} />
+      </Routes>
+    </div>
   )
 }
 
